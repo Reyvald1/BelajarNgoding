611c1588820b5a3df180ece24c076e89716276d9
Reyvald1 <166040833+Reyvald1@users.noreply.github.com>
2024-04-19 10:08:31 +0700

    Add files via upload

---
+++ yok.js
@@ -0,0 +1,10 @@
+// Mendeklarasikan variabel
+let pesan = 'Halo, dunia!';
+
+// Menampilkan pesan ke konsol
+console.log(pesan); // Output: Halo, dunia!
+
+/* Komentar multi-baris:
+   Kode di bawah ini tidak akan dieksekusi karena telah dikomentari.
+   Gunakan ini untuk menjelaskan bagian yang lebih besar dari kode Anda atau untuk menonaktifkan kode sementara. */
+
